459f282950673b84d29310509a4899b937e6fbcf
Daniel Szlicht <daniel.szlicht25@gmail.com>
2023-01-09 21:02:21 +0100

    sql create table

---
+++ src/main/java/jdbc/Book.java
@@ -0,0 +1,25 @@
+package jdbc;
+
+public class Book {
+    private int id;
+    private String title;
+    private String author;
+    private  int pages;
+
+    public Book(int id, String title, String author, int pages) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", pages=" + pages +
+                '}';
+    }
+}
--- src/main/java/jdbc/Main.java
+++ src/main/java/jdbc/Main.java
@@ -1,14 +1,28 @@
+package jdbc;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws SQLException{
         //create database books;
         //Wersja dla MySql
-        DriverManager.getConnection("jdbc:mysql://localhost:3306/books","root","TUTAJ HASLO");
-      // DriverManager.getConnection("jdbc:postgresql://localhost:5432/testdb",
-     //           "postgres", "MyPassword123");
+     //   DriverManager.getConnection("jdbc:mysql://localhost:3306/books","root","TUTAJ HASLO");
+      Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/testdb",
+                "postgres", "MyPassword123");
+      //tabela na ksiazki:
+        String createTableSql = """
+                CREATE TABLE books (
+                id int AUTO_INCREMENT PRIMARY KEY,
+                title varchar(255) NOT NULL,
+                author varchar(255) NOT NULL,
+                pages int NOT NULL
+                );
+                """;
+
+        connection.createStatement().execute(createTableSql);
+
         // "jdbc:mysql://localhost:3306/sonoo","root","root"
 
 
@@ -30,6 +44,7 @@ public class Main {
     //sterownik (maven)
     //-miejsce na pisanie kodu - main?
     //Uzyskujemy Connection przez DriverManager
+    //uwaga SQLException
 //3. Hibernate - zapisujemy OBIEKTY w bazie danych (ORM)
 //4. Spring JPA - automatyzacja pracy z Hibernate przy Spring
 
