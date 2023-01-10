2f4cc6403fe8a2289fe3aa5fafc3c8b2e08f83d8
Daniel Szlicht <daniel.szlicht25@gmail.com>
2023-01-10 18:49:12 +0100

    all CRUD + PreparedStatement

--- src/main/java/jdbc/Main.java
+++ src/main/java/jdbc/Main.java
@@ -1,9 +1,6 @@
 package jdbc;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Main {
     public static void main(String[] args) throws SQLException{ //Obsługa wyjątku!
@@ -34,28 +31,63 @@ public class Main {
 
         // connection.createStatement().execute(createTableSql);
 
-
+        //CRUD
+        //Create
         //Dodanie książki:
         Book book = new Book("Ubik", "Philip Dick", 250);
+        //SQL injection - rodzaj ataku hakerskiego
+        //Book book = new Book("a','a',1); drop database books; --", "Philip Dick", 250);
         Statement statement = connection.createStatement();
         //formatowany String - patrz reminder
         String insertBookSql = String.format(" INSERT INTO books VALUES (0,'%s','%s',%d);",
                 book.getTitle(), book.getAuthor(), book.getPages());
 
-        System.out.println(insertBookSql);
+        //System.out.println(insertBookSql);
         //statement.execute(insertBookSql);
 
        /* żeby nie trzeba było podawać ID można taką składnię:
         "INSERT INTO books(title, author, pages) VALUES ('%s','%s',%d);"*/
 
-        //update:
-        String updateBook = """
+        //Update:
+        String updateBookSql = """
                 UPDATE books
                 SET author='Philip K. Dick'
                 WHERE title='Ubik';
                 """;
 
-        statement.execute(updateBook);
+        //statement.execute(updateBookSql);
+
+        //Read
+        String selectAllSql = "SELECT * FROM books WHERE pages<275";
+
+       ResultSet resultSet = statement.executeQuery(selectAllSql);
+      while ( resultSet.next()){
+          int id = resultSet.getInt("id");
+        //  System.out.println(id );
+          String title = resultSet.getString("title");
+         // System.out.println(title);
+          String author = resultSet.getString("author");
+       //   System.out.println(author);
+          int pages = resultSet.getInt("pages");
+       //   System.out.println(pages);
+          Book dbBook = new Book(id, title, author, pages);
+          System.out.println(dbBook);
+      }
+
+      //Delete
+        String deleteSql = "DELETE FROM books WHERE id = 2";
+        //statement.execute(deleteSql);
+
+
+
+        //aby uniknąć SQL injection attack - zawsze należy używać PreparedStatement a nie Statement
+        String insertSql = "INSERT INTO books VALUES(0,?,?,?);";
+        PreparedStatement preparedStatement = connection.prepareStatement(insertSql);
+        preparedStatement.setString(1,"Czysta Architektura");
+        preparedStatement.setString(2, "Robert Martin");
+        preparedStatement.setInt(3, 350);
+        preparedStatement.execute();
+
 
     }
 }
