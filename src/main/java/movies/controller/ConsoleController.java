df2d83efed436b03beffa89b3df92e559d384642
Daniel Szlicht <daniel.szlicht25@gmail.com>
2023-01-11 20:25:28 +0100

    make use of TemplateMethod pattern to put shared code in one Controller class;

--- src/main/java/movies/controller/ConsoleController.java
+++ src/main/java/movies/controller/ConsoleController.java
@@ -1,95 +1,25 @@
 package movies.controller;
-
-import movies.model.Movie;
-import movies.service.MovieService;
-
-import java.sql.SQLException;
-import java.util.List;
+import javax.swing.*;
 import java.util.Scanner;
 
 //pozwala na interakcję z aplikacją (wprowadzanie danych, prezentacja rezultatów)
-public class ConsoleController {
-    private boolean running = true;
-   private MovieService movieService = new MovieService();  //tylko jeden service na kontroler!
-
-    public void startMenu() {
-        do {
-            menuAction();
-        } while (running);
+public class ConsoleController extends Controller{
+    void showMessage(String message) {
+        System.out.println(message);
     }
 
-    private void menuAction() {
-        showOptions();
-        int input = readDecision();
-        handleOption(input);
+    int readInt(String message) {
+        try{
+            String input = readString(message);
+            return Integer.parseInt(input);
+        }catch (NumberFormatException e){
+            showMessage( "Należy podać liczbę!");
+            return readInt(message);
+        }
     }
-
-    private void showOptions() {
-        System.out.println("""
-                Wybierz jedną z opcji:
-                1. Dodaj nowy film
-                2. Wyświetl filmy
-                3. Koniec""");
-    }
-
-    private int readDecision() {
+    String readString(String message) {
         Scanner scanner = new Scanner(System.in);
-        return scanner.nextInt();
-    }
-
-    private void handleOption(int input) {
-        try {
-            executeOption(input);
-        } catch (SQLException e) {
-            System.out.println("Błąd zapytania do BD");
-        }
-    }
-
-    private void executeOption(int input) throws SQLException{ //todo dekompozycja!
-        switch (input) {
-            case 1:
-                Movie movie = readMovieData();
-                movieService.save(movie);
-                break;
-            case 2:
-               List<Movie> movies = movieService.findAllMovies();
-               showMovies(movies);
-                break;
-            case 3:
-                end();
-                break;
-        }
-    }
-
-    private Movie readMovieData() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Podaj tytuł:");
-        String title = scanner.nextLine();
-        System.out.print("Podaj rok premiery:");
-        int premiereYear = scanner.nextInt();
-        if (premiereYear < 1800 || premiereYear > 2100) {
-            System.out.println("Podano nierealną datę premiery. " +
-                    "Powinien być przedział: 1800 - 2100");
-            return readMovieData();
-        }
-//        scanner = new Scanner(System.in);
-        scanner.nextLine();
-        System.out.print("Podaj gatunek:");
-        String genre = scanner.nextLine();
-        System.out.print("Podaj ocenę (1-5):");
-        int rate = scanner.nextInt();
-        return new Movie(title, premiereYear, genre, rate);
-    }
-
-    private void showMovies(List<Movie> movies) {
-        for (Movie movie : movies) {
-            System.out.println(movie);
-        }
-    }
-
-    private void end() throws SQLException {
-        System.out.println("Koniec");
-        running = false;
-        movieService.closeAllResources();
+        showMessage(message);
+        return scanner.nextLine();
     }
 }
---
+++ src/main/java/movies/controller/Controller.java
@@ -0,0 +1,94 @@
+package movies.controller;
+
+import movies.model.Movie;
+import movies.service.MovieService;
+
+import javax.swing.*;
+import java.sql.SQLException;
+import java.util.List;
+
+public abstract class Controller {
+    private boolean running = true;
+    private MovieService movieService = new MovieService();  //tylko jeden service na kontroler!
+    private static final String OPTIONS = """
+            Wybierz jedną z opcji:
+            1. Dodaj nowy film
+            2. Wyświetl filmy
+            3. Koniec""";
+
+    public void startMenu() {
+        do {
+            menuAction();
+        } while (running);
+    }
+
+    private void menuAction() {
+        int input = readInt(OPTIONS);
+        handleOption(input);
+    }
+
+    private void handleOption(int input) {
+        try {
+            executeOption(input);
+        } catch (SQLException e) {
+            showMessage("Błąd zapytania do BD");
+        }
+    }
+
+    private void executeOption(int input) throws SQLException {
+        switch (input) {
+            case 1:
+                addMovie();
+                break;
+            case 2:
+                showMovies();
+                break;
+            case 3:
+                end();
+                break;
+        }
+    }
+
+    private void addMovie() throws SQLException {
+        Movie movie = readMovieData();
+        movieService.save(movie);
+    }
+
+    private void showMovies() throws SQLException {
+        List<Movie> movies = movieService.findAllMovies();
+        showMovies(movies);
+    }
+
+    private Movie readMovieData() {
+        String title = readString("Podaj tytuł:");
+        int premiereYear = readInt("Podaj rok premiery:");
+        if (premiereYear < 1800 || premiereYear > 2100) {
+            showMessage("Podano nierealną datę premiery. " +
+                    "Powinien być przedział: 1800 - 2100");
+            return readMovieData();
+        }
+        String genre = readString("Podaj gatunek:");
+        int rate = readInt("Podaj ocenę (1-5):");
+        return new Movie(title, premiereYear, genre, rate);
+    }
+
+    private void showMovies(List<Movie> movies) {
+        String allFilms = "";
+        for (Movie movie : movies) {
+            allFilms += movie + "\n";
+        }
+        showMessage(allFilms);
+    }
+
+    private void end() throws SQLException {
+        showMessage("Koniec");
+        running = false;
+        movieService.closeAllResources();
+    }
+
+    abstract void showMessage(String message);
+
+    abstract int readInt(String message);
+
+    abstract String readString(String message);
+}
