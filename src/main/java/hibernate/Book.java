268b8880672b76452b06780b026d35d5f439d8c7
Daniel Szlicht <daniel.szlicht25@gmail.com>
2023-01-12 19:01:58 +0100

    hibernate configuration + first Entity

---
+++ src/main/java/hibernate/Book.java
@@ -0,0 +1,48 @@
+package hibernate;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Book {
+   @Id
+    private int id;
+    private String title;
+    private String author;
+    private  int pages;
+
+    public Book(String title, String author, int pages) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+    }
+
+    public Book(int id, String title, String author, int pages) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", pages=" + pages +
+                '}';
+    }
+}
--- src/main/java/hibernate/HibernateDemo.java
+++ src/main/java/hibernate/HibernateDemo.java
@@ -1,19 +1,34 @@
 package hibernate;
 
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateDemo {
     public static void main(String[] args) {
 
         Configuration configuration = new Configuration();
+        configuration.addAnnotatedClass(Book.class);
+        configuration.configure("hibernate.cfg.xml");
+        //SessionFactory
+       SessionFactory sessionFactory = configuration.buildSessionFactory(); //SessionFactory to odpowiednik Connection z JDBC
+        //SessionFactory - ciężki obiekt, potrzebny tylko jeden na całą aplikację
+
+        //ORM - Object-Relation Mapper
+        Book book = new Book("DDD Kompedium wiedzy","Vaughn Vernon", 130);
+
+        Session session = sessionFactory.openSession();
+        session.save(book);
+
+        //Aby klasa była encją bazodanową, rozumianą przez hibernate musi:
+        //być oznaczona adnotacją @Entity
+        //mysi być wskazana przy konfiguracji (addAnnotatedClass)
+        //musi być wskazane id adnotacją @Id
+        //oraz....
 
 
 
-
-      /*  Configuration configuration = new Configuration();
-        configuration.configure();
-        SessionFactory sessionFactory = configuration.buildSessionFactory();*/
 
     }
 }
